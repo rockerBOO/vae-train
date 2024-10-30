773d80cc504b9b90ff0744d12433d0f568cdbc48
rockerBOO <rockerboo@gmail.com>
2024-10-30 12:28:13 -0400

    Add library

--- train.py
+++ train.py
@@ -1,12 +1,9 @@
 import logging
 import math
 import os
-import threading
-import gc
 
 import lpips
 import numpy as np
-import psutil
 import torch
 import torch.nn.functional as F
 import torchvision
@@ -25,65 +22,11 @@ from peft import get_peft_model, LoraConfig
 from torchvision import transforms
 from tqdm import tqdm
 
-from trainer_args import parse_args, TrainerArgs
+from vae_train.trainer_args import parse_args, TrainerArgs
+from vae_train.trace import TorchTracemalloc, b2mb
 
 
 logger = get_logger(__name__, log_level="INFO")
-
-
-# Converting Bytes to Megabytes
-def b2mb(x):
-    return int(x / 2**20)
-
-
-# This context manager is used to track the peak memory usage of the process
-class TorchTracemalloc:
-    def __enter__(self):
-        gc.collect()
-        torch.cuda.empty_cache()
-        torch.cuda.reset_max_memory_allocated()  # reset the peak gauge to zero
-        self.used = 0.0
-        self.begin = torch.cuda.memory_allocated()
-        self.process = psutil.Process()
-
-        self.cpu_begin = self.cpu_mem_used()
-        self.peak = torch.cuda.max_memory_allocated()
-        self.peak_monitoring = True
-        peak_monitor_thread = threading.Thread(target=self.peak_monitor_func)
-        peak_monitor_thread.daemon = True
-        peak_monitor_thread.start()
-        return self
-
-    def cpu_mem_used(self):
-        """get resident set size memory for the current process"""
-        return self.process.memory_info().rss
-
-    def peak_monitor_func(self):
-        self.cpu_peak = -1
-
-        while True:
-            self.cpu_peak = max(self.cpu_mem_used(), self.cpu_peak)
-
-            # can't sleep or will not catch the peak right (this comment is here on purpose)
-            # time.sleep(0.001) # 1msec
-
-            if not self.peak_monitoring:
-                break
-
-    def __exit__(self, *exc):
-        self.peak_monitoring = False
-
-        gc.collect()
-        torch.cuda.empty_cache()
-        self.end = torch.cuda.memory_allocated()
-        self.peak = torch.cuda.max_memory_allocated()
-        self.used = b2mb(self.end - self.begin)
-        self.peaked = b2mb(self.peak - self.begin)
-
-        self.cpu_end = self.cpu_mem_used()
-        self.cpu_used = b2mb(self.cpu_end - self.cpu_begin)
-        self.cpu_peaked = b2mb(self.cpu_peak - self.cpu_begin)
-        # print(f"delta used/peak {self.used:4d}/{self.peaked:4d}")
 
 
 def prepare_dataset(args: TrainerArgs, accelerator):
